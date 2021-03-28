ec6cf2e9788d1e1b240ced7d36a845fa742c2680
Leonardo Schmitt <leonardox.schmitt@gmail.com>
2021-03-28 13:07:29 -0300

    Finalizado Pagamentos no Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,42 @@
+import { signIn, useSession } from 'next-auth/client';
 import styles from './styles.module.scss'
+import {api} from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js';
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+    const [session] = useSession()
+    
+    async function handleSubscribe(){
+        if (!session){
+            signIn('github')
+            return;
+        }
+
+        // criação da checkou session
+        try{
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId})
+
+        } catch (err) {
+            console.log(err)
+            alert(err.message)
+        }
+    }
+    
     return(
         <button
          type = 'button'
          className = {styles.subscribeButton}
+         onClick = {handleSubscribe}
         >
             Inscreva-se agora!
         </button>
